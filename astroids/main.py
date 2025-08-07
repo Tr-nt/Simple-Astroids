2338a941b8597c00db603721e55eba275b55ccd4
Tr-nt <Tdshanepowell@gmail.com>
2025-08-06 20:37:20 -0400

    Added frame rate

--- astroids/main.py
+++ astroids/main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import *
 
 def main():
+    #physics
+    game_speed = pygame.time.Clock()
+    dt = 0
 
 
     run_game = True #Flag for gamestate loop
@@ -16,11 +19,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while run_game:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
-        pygame.display.flip()
+
+        screen.fill(000) #black screen
+        game_speed.tick(60) #gives us the frame rate
+        dt = game_speed.tick(60)/1000 #will be used later
+
+        pygame.display.flip() #refreshes screen
 
 
 if __name__ == "__main__":
