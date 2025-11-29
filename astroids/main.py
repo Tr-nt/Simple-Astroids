da72b5eb41b365e6f1060e4d1dd3eeb9316a8897
Tr-nt <Tdshanepowell@gmail.com>
2025-11-28 20:54:17 -0500

    added collision for shots and asteroids

--- astroids/main.py
+++ astroids/main.py
@@ -61,6 +61,11 @@ def main():
         player.shoot_cooldown_timer -= dt
         #player.update(dt)
         for asteroid in asteroids:
+            for shot in shots: 
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
             if asteroid.collides_with(player):
                 log_event("player_hit")
                 run_game = False
